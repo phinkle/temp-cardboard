0cc820def36986c4f304e1f4bad3c2c1d6a70369
Paige Hinkle <paigehinkle@utexas.edu>
2014-12-03 00:17:02 -0600

    debugging zoom error

--- cardboard-ply/js/main.js
+++ cardboard-ply/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var camera, scene, renderer;
+var camera, scene, renderer, mesh;
 var effect, controls;
 var element, container;
 
@@ -9,9 +9,6 @@ var clock = new THREE.Clock();
 init();
 animate();
 
-/**
- * Initialize the scene for threejs by creating a WebGL renderer.
- */
 function init() {
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColorHex( 0xa3a3a3, 1 );
@@ -19,78 +16,34 @@ function init() {
   container = document.getElementById('canvas');
   container.appendChild(element);
 
-  // Create the Stereo effect for viewing in Google Cardboard.
   effect = new THREE.StereoEffect(renderer);
 
   scene = new THREE.Scene();
 
-  // Create a Perspective camera and initialize the position.
   camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-  camera.position.set(80, 80, 200);
+  camera.position.set(0, 10, 0);
   scene.add(camera);
 
-  /**
-   * Assign orbit controls to the camera.
-   * 
-   */
-  controls = new THREE.OrbitControls(camera, element);
-  controls.rotateUp(Math.PI / 8);
-  controls.panUp(Math.PI / 4);
-  controls.target.set(
-    camera.position.x + 0.1,
-    camera.position.y,
-    camera.position.z
-  );
-  controls.noZoom = true;
-  controls.noPan = true;
-  controls.autoRotate = false;
 
-  /**
-   * To move the object using the orbit controls pass in a THREE.Object3D to the constructor.
-   * This assignment causes the view to be stuck looking at the ground on the web browser, but works correctly on mobile.
-   * Must connect and update DeviceOrientationControls or the scene will not render correctly.
-   */
-  controls = new THREE.DeviceOrientationControls(camera, true);
-  controls.connect();
-  controls.update();
-
-  /**
-   * Change orientation on click events as well as on device orientation movement.
-   */
   function setOrientationControls(e) {
     if (!e.alpha) {
       return;
     }
+
     element.addEventListener('click', fullscreen, false);
 
     window.removeEventListener('deviceorientation', setOrientationControls);
   }
   window.addEventListener('deviceorientation', setOrientationControls, true);
 
-  addLights();
-  createGroundPlane();
-  displayPoints("ply/batman.ply");
 
-  window.addEventListener('resize', resize, false);
-  setTimeout(resize, 1);
-}
-
-/**
- * Create a Hemisphere light and a directional light to the scene.
- */
-function addLights() {
-    var light = new THREE.HemisphereLight(0xffffff, 0x000000, 0.3);
+  var light = new THREE.HemisphereLight(0xffffff, 0x000000, 0.3);
   scene.add(light);
 
   var dirLight = new THREE.DirectionalLight(0xffffff);
   dirLight.position.set( 0, 0, 1);
   scene.add(dirLight);
-}
 
-/**
- * Add a ground plane to the scene with a checkerboard pattern.
- */
-function createGroundPlane() {
   var texture = THREE.ImageUtils.loadTexture(
     'textures/patterns/checker.png'
   );
@@ -109,57 +62,36 @@ function createGroundPlane() {
 
   var geometry = new THREE.PlaneGeometry(1000, 1000);
 
+  // adding ground plane
   var m = new THREE.Mesh(geometry, material);
   m.rotation.x = -Math.PI / 2;
   scene.add(m);
-}
 
-/**
- * Takes in a file path of a ply file that will be displayed on the screen.
- */
-function displayPoints(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open("GET", file, false);
-  rawFile.onreadystatechange = function () {
-      if (rawFile.readyState === 4) {
-          if (rawFile.status === 200 || rawFile.status == 0) {
-              var allText = rawFile.responseText;
-              parsePoints(allText);
-          }
-      }
-  }
-  rawFile.send(null);
-}
+  // loading ply file
+  var loader = new THREE.PLYLoader();
+    loader.addEventListener( 'load', function ( event ) {
+    console.log("found ply file");
 
-/**
- * Takes in the coordinates and colors of a ply file as a string.
- * Parses through the points and created a point cloud from the data.
- */
-function parsePoints(data) {
-  var pointSize = 1.0;
-  var lines = data.split("\n");
-  var geometry = new THREE.Geometry();
+    var geometry = event.content;
+    var material = new THREE.MeshPhongMaterial( { ambient: 0x0055ff, color: 0x0055ff, specular: 0x111111, shininess: 200 } );
+    mesh = new THREE.Mesh( geometry, material );
+    mesh.name = "ply";
+    controls = new THREE.DeviceOrientationControls(mesh, true);
+    controls.connect();
+    controls.update();
 
-  var colors = [];
+    mesh.position.set( 0, 10, -25);
+    mesh.rotation.set( 0, - Math.PI / 2, 0 );
+    mesh.scale.set( 0.05, 0.05, 0.05 );
 
-  for (var i = 0; i < lines.length - 1; i++) {
-    var points = lines[i].split(" ");
+    scene.add(mesh);
 
-    /** Threejs/ply coordinate system differences
-     *  x and y are switched.
-     *  y is upside down. Multiply by -1 and add a buffer.
-     */
-    var vector = new THREE.Vector3(points[1] - 200, (points[0] * -1) + 350, points[2]);
-    geometry.vertices.push(vector);
-    colors[i] = new THREE.Color("rgb(" + points[5] + "," + points[4] + "," + points[3] + ")" );
-  }
+  } );
+  loader.load( 'ply/dolphins.ply' );
 
-  geometry.colors = colors;
-  geometry.computeBoundingBox();
 
-  var material = new THREE.PointCloudMaterial( { size: pointSize, vertexColors: THREE.VertexColors } );
-  var pointcloud = new THREE.PointCloud( geometry, material);
-  scene.add(pointcloud);
+  window.addEventListener('resize', resize, false);
+  setTimeout(resize, 1);
 }
 
 function resize() {
