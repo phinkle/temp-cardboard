3042d1e8d2e7a6a4dc802d2945fc49806023f082
Paige Hinkle <paigehinkle@utexas.edu>
2014-12-03 11:54:20 -0600

    revert

--- cardboard-ply/js/main.js
+++ cardboard-ply/js/main.js
@@ -9,29 +9,21 @@ var clock = new THREE.Clock();
 init();
 animate();
 
-/**
- * Initialize the scene for threejs by creating a WebGL renderer.
- */
 function init() {
   renderer = new THREE.WebGLRenderer();
-  renderer.setClearColorHex(0xa3a3a3, 1);
+  renderer.setClearColorHex( 0xa3a3a3, 1 );
   element = renderer.domElement;
   container = document.getElementById('canvas');
   container.appendChild(element);
 
-  // Create the Stereo effect for viewing in Google Cardboard.
   effect = new THREE.StereoEffect(renderer);
 
   scene = new THREE.Scene();
 
-  // Create a Perspective camera and initialize the position.
   camera = new THREE.PerspectiveCamera(90, 1, 0.001, 700);
-  camera.position.set(0, 80, 0);
+  camera.position.set(80, 80, 200);
   scene.add(camera);
 
-  /**
-   * Assign orbit controls to the camera.
-   */
   controls = new THREE.OrbitControls(camera, element);
   controls.rotateUp(Math.PI / 8);
   controls.panUp(Math.PI / 4);
@@ -44,18 +36,10 @@ function init() {
   controls.noPan = true;
   controls.autoRotate = false;
 
-  /**
-   * To move the object using the orbit controls pass in a THREE.Object3D to the constructor.
-   * This assignment causes the view to be stuck looking at the ground on the web browser, but works correctly on mobile.
-   * Must connect and update DeviceOrientationControls or the scene will not render correctly.
-   */
   controls = new THREE.DeviceOrientationControls(camera, true);
-  controls.connect();
-  controls.update();
+    controls.connect();
+    controls.update();
 
-  /**
-   * Change orientation on click events as well as on device orientation movement.
-   */
   function setOrientationControls(e) {
     if (!e.alpha) {
       return;
@@ -66,31 +50,14 @@ function init() {
   }
   window.addEventListener('deviceorientation', setOrientationControls, true);
 
-  addLights();
-  createGroundPlane();
-  createPointCloud("ply/batman.ply");
 
-  window.addEventListener('resize', resize, false);
-  setTimeout(resize, 1);
-}
-
-/**
- * Create a Hemisphere light and a directional light to the scene.
- */
-function addLights() {
-  var hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.3);
-  scene.add(hemiLight);
+  var light = new THREE.HemisphereLight(0xffffff, 0x000000, 0.3);
+  scene.add(light);
 
   var dirLight = new THREE.DirectionalLight(0xffffff);
-  dirLight.position.set(0, 0, 1);
+  dirLight.position.set( 0, 0, 1);
   scene.add(dirLight);
-}
 
-/**
- * Add a ground plane to the scene with a checkerboard pattern.
- */
-function createGroundPlane() {
-  var groundSize = 1000;
   var texture = THREE.ImageUtils.loadTexture(
     'textures/patterns/checker.png'
   );
@@ -107,23 +74,47 @@ function createGroundPlane() {
     map: texture
   });
 
-  var geometry = new THREE.PlaneGeometry(groundSize, groundSize);
+  var geometry = new THREE.PlaneGeometry(1000, 1000);
 
-  var mesh = new THREE.Mesh(geometry, material);
-  mesh.rotation.x = -Math.PI / 2;
-  scene.add(mesh);
+  // adding ground plane
+  var m = new THREE.Mesh(geometry, material);
+  m.rotation.x = -Math.PI / 2;
+  scene.add(m);
+
+  var mesh;
+  displayPoints("ply/batman.ply");
+
+  // loading ply file
+//   var loader = new THREE.PLYLoader();
+//         loader.addEventListener( 'load', function ( event ) {
+//           console.log("found ply file");
+
+//           var geometry = event.content;
+//           var material = new THREE.MeshPhongMaterial( { ambient: 0x0055ff, color: 0x0055ff, specular: 0x111111, shininess: 200 } );
+//           mesh = new THREE.Mesh( geometry, material );
+//           controls = new THREE.DeviceOrientationControls(camera, true);
+
+//           mesh.position.set( 5, 10, 0);
+//           mesh.rotation.set( 0, - Math.PI / 2, 0 );
+//           mesh.scale.set( 50, 50, 50 );
+
+//           scene.add(mesh);
+
+//         } );
+//         loader.load( 'ply/pikachu.ply' );
+
+  window.addEventListener('resize', resize, false);
+  setTimeout(resize, 1);
 }
 
-/**
- * Takes in a file path of a ply file that will be displayed on the screen.
- */
-function createPointCloud(file) {
+function displayPoints(file) {
   var rawFile = new XMLHttpRequest();
   rawFile.open("GET", file, false);
   rawFile.onreadystatechange = function () {
       if (rawFile.readyState === 4) {
           if (rawFile.status === 200 || rawFile.status == 0) {
               var allText = rawFile.responseText;
+              // console.log(allText);
               parsePoints(allText);
           }
       }
@@ -131,43 +122,30 @@ function createPointCloud(file) {
   rawFile.send(null);
 }
 
-/**
- * Takes in the coordinates and colors of a ply file as a string.
- * Parses through the points and created a point cloud from the data.
- */
 function parsePoints(data) {
-  var pointSize = 1.0;
-  var xOffset = -200;
-  var yOffset = 350;
-  var zOffset = 0;
   var lines = data.split("\n");
   var geometry = new THREE.Geometry();
 
   var colors = [];
 
   for (var i = 0; i < lines.length - 1; i++) {
+    //console.log(lines[i]);
     var points = lines[i].split(" ");
 
-    /** Threejs/ply coordinate system differences:
-     *  x and y are switched.
-     *  y is upside down. Multiply by -1 and add a buffer.
-     */
-    var vector = new THREE.Vector3(points[1] + xOffset, (points[0] * -1) + yOffset, points[2] + zOffset);
+    var vector = new THREE.Vector3(points[1] - 200, (points[0] * -1) + 350, points[2]);
     geometry.vertices.push(vector);
-    colors[i] = new THREE.Color("rgb(" + points[5] + "," + points[4] + "," + points[3] + ")" );
+    var c = new THREE.Color("rgb(" + points[5] + "," + points[4] + "," + points[3] + ")" );
+    colors[i] = c;
   }
 
   geometry.colors = colors;
   geometry.computeBoundingBox();
 
-  var material = new THREE.PointCloudMaterial({size: pointSize, vertexColors: THREE.VertexColors});
-  var pointcloud = new THREE.PointCloud(geometry, material);
+  var material = new THREE.PointCloudMaterial( { size: 1.0, vertexColors: THREE.VertexColors } );
+  var pointcloud = new THREE.PointCloud( geometry, material );
   scene.add(pointcloud);
 }
 
-/**
- * Resize the canvas when the screen is resized.
- */
 function resize() {
   var width = container.offsetWidth;
   var height = container.offsetHeight;
@@ -179,27 +157,18 @@ function resize() {
   effect.setSize(width, height);
 }
 
-/**
- * Update the controls and projection matrix.
- */
-function update(deltaT) {
+function update(dt) {
   resize();
 
   camera.updateProjectionMatrix();
 
-  controls.update(deltaT);
+  controls.update(dt);
 }
 
-/**
- * Render the scene using the stereo effect with the current scene and camera.
- */
-function render(deltaT) {
+function render(dt) {
   effect.render(scene, camera);
 }
 
-/**
- * Animate the canvas by updating and rendering for each frame.
- */
 function animate(t) {
   requestAnimationFrame(animate);
 
@@ -207,9 +176,6 @@ function animate(t) {
   render(clock.getDelta());
 }
 
-/**
- * Handle fullscreen display.
- */
 function fullscreen() {
   if (container.requestFullscreen) {
     container.requestFullscreen();
