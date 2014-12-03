361b0504f4b7d83a2aea98bf737ff129e1696c0d
Paige Hinkle <paigehinkle@utexas.edu>
2014-12-03 12:16:59 -0600

    got it

--- cardboard-ply/js/main.js
+++ cardboard-ply/js/main.js
@@ -153,7 +153,7 @@ function parsePoints(data) {
      *  x and y are switched.
      *  y is upside down. Multiply by -1 and add a buffer.
      */
-    var vector = new THREE.Vector3(points[1] - 200, (points[0] * -1) + yOffset, points[2]);
+    var vector = new THREE.Vector3(points[1] + xOffset, (points[0] * -1) + yOffset, points[2] + zOffset);
     geometry.vertices.push(vector);
     var c = new THREE.Color("rgb(" + points[5] + "," + points[4] + "," + points[3] + ")" );
     colors[i] = c;
