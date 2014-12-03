997b097a70e8df9810c54d92607e820fe4285ddf
Paige Hinkle <paigehinkle@utexas.edu>
2014-12-03 12:29:14 -0600

    not liking that

--- cardboard-ply/js/main.js
+++ cardboard-ply/js/main.js
@@ -138,7 +138,7 @@ function displayPoints(file) {
  */
 function parsePoints(data) {
   var pointSize = 1.0;
-  var xOffset = -200.0;
+  var xOffset = 200.0;
   var yOffset = 350.0;
   var lines = data.split("\n");
   var geometry = new THREE.Geometry();
@@ -152,7 +152,7 @@ function parsePoints(data) {
      *  x and y are switched.
      *  y is upside down. Multiply by -1 and add a buffer.
      */
-    var vector = new THREE.Vector3(points[1] + xOffset, (points[0] * -1) + yOffset, points[2]);
+    var vector = new THREE.Vector3(points[1] - xOffset, (points[0] * -1) + yOffset, points[2]);
     geometry.vertices.push(vector);
     var c = new THREE.Color("rgb(" + points[5] + "," + points[4] + "," + points[3] + ")" );
     colors[i] = c;
